f591d103c2700bbaff05bc986ece1d903e4f2ee1
Asad Tariq <asad.tariq@unity3d.com>
2023-04-17 15:58:01 +0300

    add support for symmetric jws, add xo

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-const express = require("express");
+const express = require('express');
+const { expressValidateJWS } = require('@unity/node-jws-validator');
+
+const secret = 'test-secret';
+
 const app = express();
 const PORT = 4000;
 
-app.get("/", (req, res) => {
-  res.json({ status: "ok" });
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
 });
 
-app.all("/v1/status/:status/delayed/:ms", (req, res) => {
-  res.status(req.params.status);
-  setTimeout(() => {
-    res.json({
-      name: "Fake Backend Response",
-      statusCode: req.params.status,
-      delay: req.params.ms,
-    });
-  }, req.params.ms);
-});
+app.all(
+  '/v1/status/:status/delayed/:ms',
+  expressValidateJWS(secret),
+  (req, res) => {
+    res.status(req.params.status);
+    setTimeout(() => {
+      res.json({
+        name: 'Fake Backend Response',
+        statusCode: req.params.status,
+        delay: req.params.ms,
+      });
+    }, req.params.ms);
+  },
+);
 
 app.post(
-  "/v1/upload/status/:status/delayed/:ms/size/:expectedContentLength",
+  '/v1/upload/status/:status/delayed/:ms/size/:expectedContentLength',
   (req, res) => {
     let payloadSize = 0;
 
-    req.on("end", () => {
+    req.on('end', () => {
       const expectedLength = Number.parseInt(
         req.params.expectedContentLength,
-        10
+        10,
       );
-      if (expectedLength) {
-        if (expectedLength !== payloadSize) {
-          res.status(400);
-          res.setHeader('content-type', 'application/problem+json');
-          res.json({
-            error: `Expected length ${expectedLength} is not equal to payload size ${payloadSize}`,
-          });
-          return;
-        }
+      if (expectedLength && expectedLength !== payloadSize) {
+        res.status(400);
+        res.setHeader('content-type', 'application/problem+json');
+        res.json({
+          error: `Expected length ${expectedLength} is not equal to payload size ${payloadSize}`,
+        });
+        return;
       }
 
       setTimeout(() => {
         res.status(req.params.status);
         res.json({
-          name: "Fake Backend Response",
+          name: 'Fake Backend Response',
           statusCode: req.params.status,
           delay: req.params.ms,
           fileSize: payloadSize,
@@ -49,29 +55,29 @@ app.post(
       }, req.params.ms);
     });
 
-    req.on("data", (chunk) => {
+    req.on('data', (chunk) => {
       payloadSize += chunk.length;
     });
-  }
+  },
 );
 
 app.post(
-  "/v1/upload/failed/status/:status/delayed/:ms/size/:expectedContentLength",
+  '/v1/upload/failed/status/:status/delayed/:ms/size/:expectedContentLength',
   (req, res) => {
     let payloadSize = 0;
     setTimeout(() => {
       res.status(req.params.status);
       res.json({
-        name: "Fake Backend Response",
+        name: 'Fake Backend Response',
         statusCode: req.params.status,
         delay: req.params.ms,
         fileSize: payloadSize,
       });
     }, req.params.ms);
-    req.on("data", (chunk) => {
+    req.on('data', (chunk) => {
       payloadSize += chunk.length;
     });
-  }
+  },
 );
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
